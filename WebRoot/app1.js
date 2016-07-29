fe12ab085611b702fa8552db9a9957e7127a6408
helingyu1122@qq.com <helingyu1122@qq.com>
2016-07-29 16:41:53 +0800

    对首页和登录弹出框进行了修改

--- WebRoot/app1.js
+++ WebRoot/app1.js
@@ -1,4 +1,5 @@
-﻿var routeApp = angular.module('routeApp', ["ngCookies","ui.bootstrap","ngRoute"]);
+﻿var routeApp = angular.module('routeApp', [ "ngCookies", "ui.bootstrap",
+		"ngRoute" ]);
 
 routeApp.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/logon', {
@@ -10,7 +11,7 @@ routeApp.config([ '$routeProvider', function($routeProvider) {
 	}).when('/homepage', {
 		templateUrl : 'agl_homepage.html',
 		controller : 'HomePageCtl'
-	}).when('/',{
+	}).when('/', {
 		templateUrl : 'agl_start.html',
 		controller : 'ModalController'
 	}).otherwise({
@@ -18,7 +19,8 @@ routeApp.config([ '$routeProvider', function($routeProvider) {
 	});
 } ]);
 // logonController
-routeApp.controller('LoginController', function($scope, $http, $location,$cookieStore) {
+routeApp.controller('LoginController', function($scope, $http, $location,
+		$cookieStore) {
 	$scope.errorMessage;
 	$scope.formData = {};
 	// process the form
@@ -33,7 +35,7 @@ routeApp.controller('LoginController', function($scope, $http, $location,$cookie
 			},
 		}).success(function(data) {
 			if (data.state) {
-				$cookieStore.put("username",data.message);
+				$cookieStore.put("username", data.message);
 				$http({
 					method : 'POST',
 					url : 'timeline/showFocusTL.do',
@@ -41,10 +43,10 @@ routeApp.controller('LoginController', function($scope, $http, $location,$cookie
 						u_name : data.message
 					},
 				}).success(function(data) {
-					$cookieStore.put("blogs",data);
+					$cookieStore.put("blogs", data);
 				});
-				
-				//alert("成功啦~~~~~");
+
+				// alert("成功啦~~~~~");
 				$location.path('/homepage');
 			} else {
 				$scope.errorMessage = data.message;
@@ -52,73 +54,108 @@ routeApp.controller('LoginController', function($scope, $http, $location,$cookie
 		});
 	};
 });
-//RegisterController
-routeApp.controller('RegisterController',function($scope,$http,$location,$cookieStore){
-	$scope.formData={};
-	$scope.processForm=function(){
+// RegisterController
+routeApp.controller('RegisterController', function($scope, $http, $location,
+		$cookieStore) {
+	$scope.formData = {};
+	$scope.processForm = function() {
 		$http({
 			method : 'POST',
 			url : 'user/register.do',
 			data : {
 				u_name : $scope.formData.name,
 				u_pwd : $scope.formData.pwd,
-				u_nick: $scope.formData.nickname
+				u_nick : $scope.formData.nickname
 			},
 		}).success(function(data) {
-			if(data.state){
-				$cookieStore.put("username",data.message);
+			if (data.state) {
+				$cookieStore.put("username", data.message);
 				$location.path('/homepage');
 			}
 		});
 	};
 });
-//HomePageController
-routeApp.controller('HomePageCtl', function($scope,$cookieStore,$http) {
+// HomePageController
+routeApp.controller('HomePageCtl', function($scope, $cookieStore, $http) {
 	$scope.username = $cookieStore.get("username");
 	$scope.data = "";
 	$scope.blogs = $cookieStore.get("blogs");
-	$scope.processBlog = function(){
+	$scope.processBlog = function() {
 		$http({
 			method : 'POST',
 			url : 'blog/publish.do',
 			data : {
-				b_uname: $scope.username,
-				b_content: $scope.data,
-				b_isTurn: 0,
-				b_time: 0,
-				b_turn: 0,
-				b_keep: 0,
-				b_comment: 0
+				b_uname : $scope.username,
+				b_content : $scope.data,
+				b_isTurn : 0,
+				b_time : 0,
+				b_turn : 0,
+				b_keep : 0,
+				b_comment : 0
 			},
 		}).success(function(data) {
-			if(data.state){
+			if (data.state) {
 				alert("发布成功");
 				$scope.data = "";
 			}
 		});
 	};
 });
-routeApp.controller('ModalController',function($scope,$modal,$location){
-	$scope.addModal = function(){
-		alert("aka test");
+routeApp.controller('ModalController', function($scope, $modal, $location) {
+	$scope.addModal = function() {
 		var logonModal = $modal.open({
 			templateUrl : 'myModelContent.html',
+			controller : 'ModalInstanceController',
 			size : 'lg'
-			
+
 		});
 	};
+	$scope.register = function(){
+		alert("注册");
+		$location.path('/register');
+		
+	};
 });
-routeApp.controller('ModalInstanceController',function($scope,$modalInstance){
-	
-	$scope.ok = function() {
-	    alert("哈喽");
-	  };
-	
-	$scope.cancel = function() {
-	    $modalInstance.dismiss('cancel');
-	  };
-	
-});
+routeApp.controller('ModalInstanceController',
+		function($scope, $modalInstance,$http,$cookieStore,$location) {
+			$scope.formData = {};
+
+			$scope.ok = function() {
+				$http({
+					method : 'POST',
+					url : 'user/logon.do',
+					type : 'text',
+					data : {
+						u_name : $scope.formData.name,
+						u_pwd : $scope.formData.pwd
+					},
+				}).success(function(data) {
+					if (data.state) {
+						$cookieStore.put("username", data.message);
+						$http({
+							method : 'POST',
+							url : 'timeline/showFocusTL.do',
+							data : {
+								u_name : data.message
+							},
+						}).success(function(data) {
+							$cookieStore.put("blogs", data);
+						});
+
+						// alert("成功啦~~~~~");
+						$location.path('/homepage');
+						$modalInstance.dismiss('cancel');
+					} else {
+						$scope.errorMessage = data.message;
+					}
+				});
+			};
+
+			$scope.cancel = function() {
+				$modalInstance.dismiss('cancel');
+			};
+
+		});
 routeApp.controller('RouteListCtl', function($scope) {
 });
 routeApp.controller('RouteDetailCtl', function($scope, $routeParams) {
